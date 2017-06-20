ded56f8d0ab6cf05faaaece7ae466c0a86f97f5e
Aleksandr Uvarov <a.uvarov@corp.mail.ru>
2017-06-20 18:00:27 +0300

    cabin column

--- titanic.py
+++ titanic.py
@@ -10,7 +10,7 @@ y = data.Survived
 
 ## подготовка данных
 # Name
-titles_series = X.Name.str.extract('(\w+)(?=\.)')
+titles_series = X.Name.str.extract('(\w+)(?=\.)', expand=False)
 X_titles = pd.get_dummies(titles_series, prefix='Title')
 X = pd.concat((X, X_titles), axis=1)
 
@@ -20,11 +20,16 @@ X.Sex = X.Sex.map({'male' : 0, 'female' : 1})
 # Age
 X.Age = X.Age.fillna(X.Age.mean())
 
-# Cabin
+# SibSp + Parch
 # TODO
+
+# Cabin
+cabin_series = X.Cabin.str.extract('([a-zA-Z])', expand=False)
+X_cabin = pd.get_dummies(cabin_series, prefix='Cabin')
+X_cabin = X_cabin.drop('Cabin_T', axis=1)
 
 # Embarked
 X_embarked = pd.get_dummies(X.Embarked, prefix='Embarked')
 
 
-X = pd.concat((X.drop(['Name', 'Embarked', 'Ticket'], axis=1), X_titles, X_embarked), axis=1)
+X = pd.concat((X.drop(['Name', 'Embarked', 'Ticket', 'Cabin'], axis=1), X_titles, X_embarked, X_cabin), axis=1)
